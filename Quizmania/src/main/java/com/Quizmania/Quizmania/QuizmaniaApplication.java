bef06753678ef408647897a6fddd4ff9627a36f9
k.ziolkowski321 <k.ziolkowski321@gmail.com>
2022-03-24 16:50:40 +0100

    Start

---
+++ Quizmania/src/main/java/com/Quizmania/Quizmania/QuizmaniaApplication.java
@@ -0,0 +1,13 @@
+package com.Quizmania.Quizmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmaniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmaniaApplication.class, args);
+	}
+
+}
