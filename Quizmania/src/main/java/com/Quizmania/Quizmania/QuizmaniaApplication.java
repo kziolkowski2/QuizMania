14859aa6dae839a550827da424fc7d52bd340990
k.ziolkowski321 <k.ziolkowski321@gmail.com>
2022-03-28 18:57:40 +0200

    Changes

--- Quizmania/src/main/java/com/Quizmania/Quizmania/QuizmaniaApplication.java
+++ Quizmania/src/main/java/com/Quizmania/Quizmania/QuizmaniaApplication.java
@@ -1,13 +1,21 @@
 package com.Quizmania.Quizmania;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuizmaniaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizmaniaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner setUpApp(@Autowired QuizService quizService) {
+		return (args) -> quizService.initialize();
 	}
 
 }
